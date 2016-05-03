827888d419502fffa78df374866d355fe1fa7fe7
satish mhatre <satishpmhatre@gmail.com>
2016-05-03 17:28:06 +0530

    commit

--- folder7/marks.java
+++ folder7/marks.java
@@ -4,6 +4,7 @@ public class marks {
 
    public static void main(String[] args) {
       Scanner s = new Scanner ( System.in);
+	  //This is first commit
       System.out.print("Enter your name: ");
       String name=s.next();
 	  System.out.print("Enter your surname: ");
